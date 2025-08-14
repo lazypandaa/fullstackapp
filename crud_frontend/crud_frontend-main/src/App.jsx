902de1c3e4e5270b7f618e1d99a4f87e859514bc
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2025-08-14 12:17:10 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://54.158.7.152:9090/springapp1';
+const BASE_URL = 'http://98.81.3.128:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
