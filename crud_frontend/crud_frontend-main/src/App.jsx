d4bd0452cbeffd5b06fdad48cb5d7fbfd8e52b2a
K Eshwar Krishna <72182474+lazypandaa@users.noreply.github.com>
2025-08-25 11:57:19 +0530

    Update BASE_URL to new server address

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://98.81.3.128:9090/springapp1';
+const BASE_URL = 'http://13.216.249.223:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
